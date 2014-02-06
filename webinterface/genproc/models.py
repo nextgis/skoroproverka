01ee6d3daa498d66d64ae3be16974c766c496185
Dmitry Kolesov <kolesov.dm@gmail.com>
2014-02-06 16:54:34 +0400

    Добавление полнотекстовых индексов в модель

--- webinterface/genproc/models.py
+++ webinterface/genproc/models.py
@@ -4,8 +4,12 @@ from sqlalchemy import (
     Column,
     Index,
     Integer,
-    Text
+    Text,
+    event,
+    DDL
 )
+
+from sqlalchemy.types import UserDefinedType
 
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -19,9 +23,30 @@ from zope.sqlalchemy import ZopeTransactionExtension
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
+###########################################################################################
+# Поддержка полнотекстового поиска.
+# В основе реализации лежит статья:
+#   http://nibrahim.net.in/2013/11/29/sqlalchemy_and_full_text_searching_in_postgresql.html
+###########################################################################################
+
+class TsVector(UserDefinedType):
+    "Holds a TsVector column"
+
+    name = "TSVECTOR"
+
+    def get_col_spec(self):
+        return self.name
+
+
+
+###########################################################################################
+# Описание основной базы:
+###########################################################################################
 
 class Genproc(Base):
     __tablename__ = 'genproc'
+    __table_args__ = (Index('details_tsvector_idx', 'details_tsvector', postgresql_using = 'gin'),)
+
     id = Column(Integer, primary_key=True)
 
     # Далее идут оригинальные поля,
@@ -47,4 +72,16 @@ class Genproc(Base):
     check_hours = Column(Text)
     check_form = Column(Text)
     check_org = Column(Text)
+
+    details_tsvector = Column(TsVector)
+
+# Триггер на таблицу genproc
+trigger_snippet = DDL("""
+    CREATE TRIGGER details_tsvector_update BEFORE INSERT OR UPDATE
+    ON genproc
+    FOR EACH ROW EXECUTE PROCEDURE
+    tsvector_update_trigger(details_tsvector,'pg_catalog.russian', 'name')
+""")
+
+event.listen(Genproc.__table__, 'after_create', trigger_snippet.execute_if(dialect = 'postgresql'))
 
